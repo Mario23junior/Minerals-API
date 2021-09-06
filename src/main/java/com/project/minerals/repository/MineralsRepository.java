ff9e8af4c68d7370720ac33b65ef530fff7b0256
Mario23junior <mariojunior3251@gmail.com>
2021-09-06 05:02:19 -0300

    Criando repository da entidade Mineral

---
+++ src/main/java/com/project/minerals/repository/MineralsRepository.java
@@ -0,0 +1,9 @@
+package com.project.minerals.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.minerals.model.Mineral;
+
+public interface MineralsRepository extends JpaRepository<Mineral, Long> { 
+
+}
