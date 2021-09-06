0dab4c0a206a688b2b4beed953785714baa525c9
Mario23junior <mariojunior3251@gmail.com>
2021-09-06 05:08:22 -0300

    Criando repository da entidade cristalografia

---
+++ src/main/java/com/project/minerals/repository/CristalografiaRepository.java
@@ -0,0 +1,9 @@
+package com.project.minerals.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.minerals.model.Cristalografia;
+
+public interface CristalografiaRepository extends JpaRepository<Cristalografia, Long> {
+
+}
