a6b1d58aa997f787a053fe6ecb0c3c17e5b6ca38
Mario23junior <mariojunior3251@gmail.com>
2021-09-07 04:49:20 -0300

    Criando camada de controller e service de cristalografia para a base de dados

--- src/main/java/com/project/minerals/controller/CristalografiaController.java
+++ src/main/java/com/project/minerals/controller/CristalografiaController.java
@@ -1,11 +1,26 @@
 package com.project.minerals.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.minerals.DTOModel.CristalografiaDTO;
+import com.project.minerals.service.ServiceCristalografia;
+
 @RestController
-@RequestMapping("/api/v1/minerais/minerals/")
+@RequestMapping("/api/v1/minerais/cristalografia/")
 public class CristalografiaController {
    
+	private ServiceCristalografia serviceCristalografia;
 	
+	public CristalografiaController(ServiceCristalografia serviceCristalografia) {
+		this.serviceCristalografia = serviceCristalografia;
+ 	}
+	
+	@PostMapping
+	public ResponseEntity<CristalografiaDTO> save(@RequestBody CristalografiaDTO cristalografiaDTO) {
+		return serviceCristalografia.saveCristalo(cristalografiaDTO);
+	}
 }
