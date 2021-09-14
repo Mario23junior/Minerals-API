dc7c84c303f78a0c2f1df46358718fd624f509db
Mario23junior <mariojunior3251@gmail.com>
2021-09-14 02:17:11 -0300

    deletando dados por id da entidade minerals da base de dados

--- src/main/java/com/project/minerals/controller/MineralController.java
+++ src/main/java/com/project/minerals/controller/MineralController.java
@@ -1,6 +1,7 @@
 package com.project.minerals.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,4 +38,21 @@ public class MineralController {
 	public ResponseEntity<MineralsDTO> update(@PathVariable Long id, @RequestBody Mineral mineral){
 		return serviceMineral.updateMinerals(id, mineral);
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Mineral> delete(@PathVariable Long id) {
+		return serviceMineral.deleteMinerals(id);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
