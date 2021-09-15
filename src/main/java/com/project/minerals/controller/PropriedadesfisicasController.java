2c1bfc74374b33f14190f3f3265963e35a2363df
Mario23junior <mariojunior3251@gmail.com>
2021-09-15 05:23:57 -0300

    Salvando informacoes da entidade propriedades fisicas no banco de dados

---
+++ src/main/java/com/project/minerals/controller/PropriedadesfisicasController.java
@@ -0,0 +1,26 @@
+package com.project.minerals.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.minerals.DTOModel.PropriedadesfisicasDTO;
+import com.project.minerals.service.ServicePropriedadesfisicas;
+
+@RestController
+@RequestMapping("/api/v1/minerais/propriedadesFisicas/")
+public class PropriedadesfisicasController {
+  
+	private ServicePropriedadesfisicas servicePropriedadesfisicas;
+	
+	public PropriedadesfisicasController(ServicePropriedadesfisicas servicePropriedadesfisicas) {
+		this.servicePropriedadesfisicas = servicePropriedadesfisicas;
+	}
+	
+	@PostMapping
+	public ResponseEntity<PropriedadesfisicasDTO> save (@RequestBody PropriedadesfisicasDTO proDto){
+		return servicePropriedadesfisicas.savePropriedadeFisica(proDto);
+	}
+}
