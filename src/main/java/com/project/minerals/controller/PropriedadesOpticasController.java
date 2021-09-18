d0c39d078001f81da29156344fb99043300b70f6
Mario23junior <mariojunior3251@gmail.com>
2021-09-18 05:11:34 -0300

    Deletando informacoes por id da entidade propriedades Opticas

--- src/main/java/com/project/minerals/controller/PropriedadesOpticasController.java
+++ src/main/java/com/project/minerals/controller/PropriedadesOpticasController.java
@@ -1,6 +1,7 @@
 package com.project.minerals.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +44,10 @@ public class PropriedadesOpticasController {
 		 return servicePropriedadesOpticas.updatePropriedadesOpticas(id, propriedadesOpticas);
 	 }
 	 
-	 
+	 @DeleteMapping("/{id}")
+	 public ResponseEntity<PropriedadesOpticas> delete(@PathVariable Long id) {
+		 return servicePropriedadesOpticas.deletePropriedadeOpti(id);
+	 }
 	 
 	 
 	 
